7cd9703b88adc1a6a9063ddf5337e373ee41ecfc
Alberto Pires de Oliveira Neto <alberto.pires.on@gmail.com>
2017-06-25 20:56:01 -0300

    added procfs support

---
+++ README.md
@@ -0,0 +1,13 @@
+## Kernel module to introduce soft lock-up/panic for testing
+
+The idea is to try to understand and if possible simulate how the *softlockup*
+condition happens in the kernel.
+
+Basic there is a busy wait loop that runs for a period of time (BUSY_LOOP) and a
+main loop (MAIN_LOOP) that loops this busy wait loop.
+
+The default initial parameters are 5 seconds for both. Those parameters can be
+changed by writing the values to the following proc filesystem files :
+
+* /proc/softlockup_test/softlockup_test_busy
+* /proc/softlockup_test/softlockup_test_main
--- softlockup_test.c
+++ softlockup_test.c
@@ -2,7 +2,7 @@
  *
  * Copyright (C) 2015  Saiyam Doshi
  *
- * 24/Jun/2016 - Updated by Alberto Pires de Oliveira Neto <alberto.pires.on@gmail.com>
+ * 25/Jun/2016 - Updated by Alberto Pires de Oliveira Neto <alberto.pires.on@gmail.com>
  *
  */
 
@@ -10,17 +10,43 @@
 #include <linux/module.h>
 #include <linux/kthread.h>
 #include <linux/delay.h>
+#include <linux/proc_fs.h>
+
+#define PROCFS_PARENT    "softlockup_test"
+#define PROCFS_BUSY_LOOP "softlockup_test_busy"
+#define PROCFS_MAIN_LOOP "softlockup_test_main"
+
+static ssize_t procfile_read(struct file *file, char *buffer, size_t length, loff_t *offset);
+static ssize_t procfile_write(struct file *file, const char *buffer, size_t length, loff_t *offset);
+static ssize_t procfile_write_main(struct file *file, const char *buffer, size_t length, loff_t *offset);
+struct proc_dir_entry *proc_parent;
+struct proc_dir_entry *proc_busy;
+struct proc_dir_entry *proc_main;
 
 struct task_struct *task0;
 static spinlock_t spinlock;
+static char msg[128];
 int val;
+int loop_seconds;
+int main_seconds;
+
+static struct file_operations file_busy_ops = {  
+	.owner = THIS_MODULE,
+	.read  = procfile_read,
+	.write = procfile_write,
+};
+
+static struct file_operations file_main_ops = {  
+	.owner = THIS_MODULE,
+	.write  = procfile_write_main,
+};
+
 
 int task(void *arg)
 {
 	struct timeval tv;
 	time_t start;
 	time_t elapsed = 0;
-	int loop;
 
 	printk(KERN_INFO "%s:%d\n",__func__,__LINE__);
 	/* To generate panic uncomment following */
@@ -34,11 +60,16 @@ int task(void *arg)
 		/* busy loop in critical section */
 		printk(KERN_INFO "%s:%d - Before\n",__func__,__LINE__);
 
-
 		for (;;) {
 			do_gettimeofday(&tv);
 			elapsed = tv.tv_sec - start;
-			if (elapsed >= 25) break;
+			if (elapsed >= loop_seconds) {
+				printk(KERN_INFO "%s:%d - Timer Value (%d)\n",__func__,__LINE__, loop_seconds);
+				do_gettimeofday(&tv);
+				start = tv.tv_sec;
+				printk(KERN_INFO "%s:%d - Reset Timer\n",__func__,__LINE__);
+				break;
+			}
 		}
 
 		msleep(0);
@@ -47,7 +78,7 @@ int task(void *arg)
 		printk(KERN_INFO "%s:%d - After\n",__func__,__LINE__);
 
 		spin_unlock(&spinlock);
-		msleep(30000);
+		msleep(main_seconds);
 	}
 
 	return val;
@@ -58,10 +89,30 @@ static int softlockup_init(void)
 	printk(KERN_INFO "%s:%d\n",__func__,__LINE__);
 
 	val = 1;
+	loop_seconds = 5;
+	main_seconds = 5000;
 	spin_lock_init(&spinlock);
 	task0 = kthread_run(&task,(void *)&val,"softlockup_thread");
 	set_cpus_allowed(task0, *cpumask_of(0));
 
+	// Proc file system
+	proc_parent = proc_mkdir(PROCFS_PARENT, NULL);
+
+	proc_busy = proc_create(PROCFS_BUSY_LOOP, S_IFREG | S_IRUGO | O_RDWR, proc_parent, &file_busy_ops);
+	if (proc_busy == NULL) {
+		remove_proc_entry(PROCFS_BUSY_LOOP, NULL);
+
+		printk(KERN_ALERT "Error: Could not initialize /proc/%s\n", PROCFS_BUSY_LOOP);
+		return -ENOMEM;
+	}
+
+	proc_main = proc_create(PROCFS_MAIN_LOOP, S_IFREG | S_IRUGO | O_RDWR, proc_parent, &file_main_ops);
+	if (proc_main == NULL) {
+		remove_proc_entry(PROCFS_MAIN_LOOP, NULL);
+
+		printk(KERN_ALERT "Error: Could not initialize /proc/%s\n", PROCFS_MAIN_LOOP);
+		return -ENOMEM;
+	}
 	return 0;
 }
 
@@ -69,6 +120,54 @@ static void softlockup_exit(void)
 {
 	printk(KERN_INFO "%s:%d\n",__func__,__LINE__);
 	kthread_stop(task0);
+	remove_proc_entry(PROCFS_BUSY_LOOP, proc_parent);
+	remove_proc_entry(PROCFS_MAIN_LOOP, proc_parent);
+	remove_proc_entry(PROCFS_PARENT   , NULL);
+}
+
+static ssize_t procfile_read(struct file *file, char *buffer, size_t length, loff_t *offset)  
+{
+	static int finished = 0;
+	int ret = 0;
+
+	printk(KERN_INFO "procfile_read (/proc/%s) called\n", PROCFS_BUSY_LOOP);
+
+	if (finished) {
+		printk(KERN_INFO "procfs_read: END\n");
+		finished = 0;
+		return 0;
+	}   
+
+	finished = 1;
+	ret = sprintf(buffer, "Hello,world!\n");
+	return ret;
+}
+
+static ssize_t procfile_write(struct file *file, const char *buffer, size_t length, loff_t *offset)  
+{
+	printk(KERN_INFO "procfile_write (/proc/%s) called\n", PROCFS_BUSY_LOOP);
+	printk(KERN_INFO "procfs_write.length : %ld\n", length);
+
+	copy_from_user(msg, buffer, length);
+	kstrtoint(msg, 10, &loop_seconds);
+	printk(KERN_INFO "Value : %d\n", loop_seconds);
+
+	return length;
+}
+
+static ssize_t procfile_write_main(struct file *file, const char *buffer, size_t length, loff_t *offset)  
+{
+	int aux;
+
+	printk(KERN_INFO "procfile_write (/proc/%s) called\n", PROCFS_MAIN_LOOP);
+	printk(KERN_INFO "procfs_write.length : %ld\n", length);
+
+	copy_from_user(msg, buffer, length);
+	kstrtoint(msg, 10, &aux);
+	main_seconds = aux * 1000;
+	printk(KERN_INFO "Value : %d\n", main_seconds);
+
+	return length;
 }
 
 module_init(softlockup_init);
